17377b7161fe7aaffd4f0160cd4e37695110881d
Trigonometry <macdevzenit@gmail.com>
2019-02-01 12:04:23 +0300

    have some errors, but work

--- src/Date_Author.java
+++ src/Date_Author.java
@@ -1,9 +1,15 @@
 import java.util.Date;
 public class Date_Author {
     public static void main(String[] args){
-        Date date =  new Date();
+        //ругается на deprecated int int int int int int int int ПАЧИМУ? ОНО ЖЫ РАБОТАЕТ
+        Date date_received =  new Date(119,1,19, 10,6);
+        String second_name = "Красногорский";
+        System.out.print("Фамилия сдающего:" + second_name);
+        System.out.print("\nЗадание получено:" + date_received.toString());
 
-        System.out.print(date.toString());
+        Date date_sended = new Date();
+        System.out.print("\nЗадание сдается: " + date_sended.toString());
+
 
     }
 }
