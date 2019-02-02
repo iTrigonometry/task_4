0e9d944880ac45eb04e8664d7bef5d64ed631f67
Trigonometry <macdevzenit@gmail.com>
2019-02-02 11:33:59 +0300

    Ready for send

--- src/max_min_div_num.java
+++ src/max_min_div_num.java
@@ -26,7 +26,25 @@ public class max_min_div_num {
             nod = nod(nod,arr[i]);
         }
 
-        System.out.print("\n" + nod);
+        System.out.print("\nНОД: " + nod);
+        int nok = 1;
+        int max = arr[0] * arr[1] / nod;
+        if (nod > 1){
+            for (int i = 1; i < n; i++){
+                if (i < n-1) {
+                    nok = arr[i] * arr[i + 1] / nod;
+                    if (nok > max)
+                        max = nok;
+                }
+            }
+            nok = max;
+        }else{
+            nok = arr[0] * arr[1] / nod(arr[1], arr[2]);
+            for (int i = 2; i < n; i++){
+                    nok = nok * arr[i] / nod(nok, arr[i]);
+            }
+        }
+        System.out.print("НОК: " + nok);
     }
 
     static int nod(int a, int b){
