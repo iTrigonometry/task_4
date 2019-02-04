11d8e73cbac852a6bc36d52a1ea2f466f337da23
iTrigonometry <34914123+iTrigonometry@users.noreply.github.com>
2019-02-04 10:31:40 +0300

    Ready for send

--- src/Period.java
+++ src/Period.java
@@ -6,25 +6,71 @@ public class Period {
 
         System.out.println("Введите количество чисел которые вы хотите ввести: ");
         int n = input.nextInt();
-        int[] arrOfNums = new int[n];
+        Integer[] arrOfNums = new Integer[n];
 
-        for(int i = 0; i < n; i++){
+        for(int index = 0; index < n; index++){
             System.out.print("Введите число: ");
-            arrOfNums[i] = input.nextInt();
+            arrOfNums[index] = input.nextInt();
         }
 
-        int index = -1;
-        for (int i = 0; i<n - 1; i++){
-            if (arrOfNums[i] > 0 && arrOfNums[i+1] > 0){
-                index = i;
+        findPeriod(arrOfNums);
+
+
+    }
+    // Algorithm @author is RodionGork
+    private static void findPeriod(Integer[] sourceNumbers) {
+        long firstNumber, secondNumber, firstTimer, length, secondTimer, index;
+        boolean isPositive = true;// если не найдем числа,то нужно об этом надо как то расказать
+        for (int n = 0; n < sourceNumbers.length - 1; n++) {
+            //проверяем длинну массива, чтобы было что искать
+            if (sourceNumbers.length < 2) {
                 break;
             }
-        }
+            //если нашли два положительных числа, то в путь
+            else if (sourceNumbers[n] >= 0 && sourceNumbers[n + 1] >= 0) {
+                isPositive = false;
+                firstNumber = sourceNumbers[n];
+                secondNumber = sourceNumbers[n + 1];
+                firstTimer = firstNumber;
+                //делаем все как в гайдиках в интернете. Числитель делим на знаменатель. При этом числитель умножаем на 10
+                for (index = 0; index < secondNumber; index++) {
+                    firstTimer = (firstTimer * 10) % secondNumber;
+                }
+                secondTimer = firstTimer;
+                length = 0;
+                // начинаем выискивать длинну периода
+                do {
+                    firstTimer = (firstTimer * 10) % secondNumber;
+                    length++;
+                } while (firstTimer != secondTimer);
+                secondTimer = firstTimer = firstNumber;
+                System.out.print("\nПериод десятичной дроби firstNumber = m/n для "
+                        + "первых двух целых положительных чисел n и m, "
+                        + "расположенных подряд: 0.");
+                for (index = 0; index < length; index++) {
+                    firstTimer = (firstTimer * 10) % secondNumber;
+                }
+                //ищем то что будет до периода и выводим
+                for (index = 0; firstTimer != secondTimer; index++) {
+                    System.out.print(secondTimer * 10 / secondNumber);
+                    firstTimer = (firstTimer * 10) % secondNumber;
+                    secondTimer = (secondTimer * 10) % secondNumber;
+                }
 
-        if (index != -1){
-
-        }else{
-            System.out.print("Дебил");
+                // выводим то что будет в скобках
+                System.out.print('(');
+                for (index = 0; index < length; index++) {
+                    System.out.print(secondTimer * 10 / secondNumber);
+                    secondTimer = (secondTimer * 10) % secondNumber;
+                }
+                //выводим дробь
+                System.out.println(')' + " = " + firstNumber + "/" + secondNumber);
+                break;
+            }else{
+                System.out.print("...");
+            }
         }
+        if (isPositive)
+            System.out.print("\nНужных чисел не найдено.");
     }
 }
